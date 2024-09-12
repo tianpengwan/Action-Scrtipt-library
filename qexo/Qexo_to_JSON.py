1244f5bc4cf436314286d3d2a7c98ac35688e453
georgezhao <georgezhao@qt-asia.com>
2024-09-12 15:29:49 +0800

    🎉 init: inital

---
+++ qexo/Qexo_to_JSON.py
@@ -0,0 +1,16 @@
+import requests 
+import json
+
+url = "https://hexo.uyoahz.cn/pub/friends/"
+response = requests.get(url)
+data = response.json()['data']  
+
+friends_circle = {"friends": []}
+for count, friend in enumerate(data):
+    friends = [friend['name'], friend['url'], friend['image']]
+    friends_circle["friends"].append(friends)
+
+friends_json = json.dumps(friends_circle)
+
+with open('friends.json', 'w') as f:
+    f.write(friends_json)
